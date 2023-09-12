6d432c405e824301cb427d820346f0d1d150c700
tangiambe <tafor@yahoo.com>
2023-09-12 16:03:36 -0400

    created home page & navbar

--- frontend/tracker-app/src/App.js
+++ frontend/tracker-app/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from "./components/Home";
+import { NavBar } from "./components/Navbar";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <NavBar/>
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
-
 export default App;
