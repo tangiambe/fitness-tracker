1785d675fa11d5213a1e98065d8131338d8d15f7
tangiambe <tafor@yahoo.com>
2023-09-18 12:30:02 -0400

    started dashboard

--- frontend/tracker-app/src/components/Dashboard.js
+++ frontend/tracker-app/src/components/Dashboard.js
@@ -1,14 +1,11 @@
 import React from "react";
-import '../styles/Home.css'
+import '../styles/Dashboard.css'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Button from "react-bootstrap/Button";
+// import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card"
 import { Table } from 'react-bootstrap'
-import { Doughnut } from 'react-chartjs-2';
-import logo from "../images/logo.svg";
-import shine from "../images/shine.svg";
 
 export const Dashboard = () => {
 
@@ -16,37 +13,29 @@ export const Dashboard = () => {
     return (
         <>
             <Container fluid id="wrapper">
-                <Col id="homePageCol">
-                    <h2 className="text-center">Welcome, User!</h2>
+                <h2 className="welcome">Welcome, User!</h2>
+                <Col>
+                    {/* <h2 className="text-center">Hi, User!</h2> */}
                     <Row>
-                        <Col>
-                            <Card body className="text-center">
-                                <h4>Daily Total Calories</h4>
-                                <h5>2500</h5>
-                                <h5>----------</h5>
-                                <h5>2500</h5>
-                                {/* <h6>{auth.userCalorieGoal.daily_calories}</h6> */}
-                            </Card>
-                        </Col>
+                        {/* <Col> */}
+                        <Card body className="Card">
+                            <h4>Daily Total Calories</h4>
+                            <h5>Total Left</h5>
+                            <h5>----------</h5>
+                            <h5>Total</h5>
+                            {/* <h6>{auth.userCalorieGoal.daily_calories}</h6> */}
+                        </Card>
+                        {/* </Col> */}
                     </Row>
                 </Col>
+                <div>
+                    <h2 className="title">Welcome, User!</h2>
+                </div>
+                {/* <Container className="Container">
 
-                <Container>
-                <Table striped bordered size="sm">
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Calories</th>
-                        <th>Fat</th>
-                        <th>Protein</th>
-                        <th>Carbs</th>
-                        <th></th>
-                    </tr>
-                    
-                </thead>
-                
-            </Table>
-            </Container>
+                    <div className="content">
+                    </div>
+                </Container> */}
 
             </Container>
         </>
