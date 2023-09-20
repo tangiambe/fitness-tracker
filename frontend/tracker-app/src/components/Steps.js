af1f61032151d33972378d27338e157555ae5301
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 <RSeymourJR04@outlook.com>
2023-09-20 09:54:07 -0400

    Blank pages for steps details and foods

---
+++ frontend/tracker-app/src/components/Steps.js
@@ -0,0 +1,13 @@
+import React from "react";
+import '../styles/Details.css'
+import Container from 'react-bootstrap/Container';
+import Button from "react-bootstrap/Button";
+
+export const Steps = () => {
+
+    return (
+        <div>
+            
+        </div>
+    );
+}
