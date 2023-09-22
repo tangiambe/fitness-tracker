d76554434fb267d9ce6cf90ca7143be14cde9a30
Vinson Martin <vm84039@yahoo.com>
2023-09-22 00:27:42 -0700

    Stylings

--- frontend/tracker-app/src/components/dashboard/BootstrapCard.js
+++ frontend/tracker-app/src/components/dashboard/BootstrapCard.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import '../../styles/Card.css';
 
 function BootstrapCard(props) {
   return (
-    <div className="card" style={{ width: '100%', margin:0 }}>
-      <div className="card-header">
-        {props.title}
+    <div className="card-dashboard" style={{ width: '100%', margin:0 }}>
+      <div className="card-body"> 
+        <div className="card-header1">
+         <h4> {props.title} </h4>
+        </div>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">{props.content}</li>
+          {/* Add more list items here if needed */}
+        </ul>
       </div>
-      <ul className="list-group list-group-flush">
-        <li className="list-group-item">{props.content}</li>
-        {/* Add more list items here if needed */}
-      </ul>
     </div>
   );
 }
